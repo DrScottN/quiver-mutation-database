c55a9ea7910cbae725db8d7a1ac9d24f4cc8aac0
Tucker Ervin <tjervin@crimson.ua.edu>
2026-03-30 14:31:07 -0400

    I realized that vars is a reserved keyword in python, so I updated both polynomial and test_polys to use variables instead. All tests pass

--- test_polys.py
+++ test_polys.py
@@ -104,7 +104,7 @@ class PolynomialVarnamesTestCase(unittest.TestCase):
         assert self.fy*0 +self.fz == self.fz
     
     def testSumVarnames(self):
-        assert self.fy + self.fz == self.fz + self.fy, f"Incorrect equality with commuting {(self.fy + self.fz).vars} vs {(self.fz + self.fy).vars}"
+        assert self.fy + self.fz == self.fz + self.fy, f"Incorrect equality with commuting {(self.fy + self.fz).variables} vs {(self.fz + self.fy).variables}"
         assert self.gw + self.fz == self.fz + self.gw
         assert self.fy + self.oney == self.fy + self.onez
         assert 3*self.fy - 2*self.fy == self.fy
