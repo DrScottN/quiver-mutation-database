5278f0f532856271dd6291da94d9a33b4fe252e6
= <=>
2026-04-28 11:22:17 -0400

    work in progress towards eval function

--- prediction.py
+++ prediction.py
@@ -14,7 +14,7 @@ def invariants_dict(Q, include_surface=True):
     invs["Seven's Congruence"] = Q.seven_congruence()
     invs["gcd"] = Q.gcd_vector() 
     if include_surface: invs["surf quiver"] = surface_quiver(Q)[0] 
-    #note, block decomp is not an invariant but the surface type is.
+    #note, block decomp is not an invariant but being surface type is
     return invs
 
 def mutation_equivalent_local(Q,R, up_to_ismorphism=False):
@@ -65,3 +65,80 @@ def mutation_acyclic_local(Q, match_alexander=False):
         return False
     else:
         return Unknown
+
+
+def run_benchmark_acyclic(dataset, train=None, use_surface=True, use_exhaustive=False, seed=2842026):
+    # Runs several variants of our benchmark against the given dataset, and prints the accuracies achieved.
+    #  dataset: iterable of tuples, (quiver, correct_label), quiver is a quiver object and correct_label is a bool
+    #  train (optional): iterable of (quiver, correct_label) pairs 
+    #   (train may be used to guess other labels, if None then some benchmarks will sample examples from dataset)
+    #  use_surface: if True, computes the block decomposition for invariants.
+    #  use_exhaustive: if True, enumerates all acyclic quivers with relevant size.
+    #  seed: used for random in selecting subset of data, irrelevant if train is not None.
+
+    dataset_copy = list(dataset) #could use itertool.tee(dataset, k) and run in parallel for memory savings
+    D = len(dataset_copy)
+    dataset_unknowns = []
+    acyclic_count = 0
+    cyclic_count = 0
+    random.seed(seed)
+    # benchmark 1: blind attempts. 
+    #  3 ways to round: unknown is incorrect; unknown as True, unknown takes False.
+    for Q,l in dataset_copy:
+        result = mutation_acyclic_local(Q, match_alexander==use_exhaustive)
+        if result is not Unknown:
+            assert result == l, f"Dataset and benchmark disagree; {Q.matrix} has label {l} but we find {result}."
+            correct += 1
+        else:
+            dataset_not_local.append((Q,l))
+            if l is True:
+                U_T_correct+=1
+            if l is False:
+                U_F_correct+=1
+        if l: 
+            acyclic_count += 1
+        else: 
+            cyclic_count += 1
+
+    print("Local tests")
+    print(f" Unknown->incorrect: {correct} ({100*correct/D}%)")
+    print(f"*Unknown->True: {correct+U_T_correct} ({100*(correct+U_T_correct)/D}%)")
+    print(f" Unknown->False: {correct+U_F_correct} ({100*(correct+U_F_correct)/D}%)")
+
+    # benchmark 2: with training data.
+    if train is None:
+        # select a random selection of quivers of each label
+        #  we choose to take samples of ~1/20 of the whole dataset; a bit weak if dataset contains few mutations from many classes
+        #  provide a comparable train set to your model otherwise.
+        acyclic_egs = random.choices([d[0] for d in dataset_copy if d[1]], k=round(acyclic_count/20))
+        cyclic_egs = random.choices([d[0] for d in dataset_copy if not d[1]], k=round(cyclic_count/20))
+    else:
+        acyclic_egs = [d[0] for d in train if d[1]]
+        cyclic_egs = [d[0] for d in train if not d[1]]
+    # build useful invariant vectors for each class
+    def get_useful_invariants(egs):
+        # return a list of distinct invariant dicts. 
+        #  for each quiver in egs, compute the invariant dict and add it to the result if it is not implied by an existing vector.
+        #  remove any dicts it implies
+        
+        #def implies(T1, T2):
+        #    # generous ternary implies; meaning T1 has more info than T2.
+        #    if T2 is Unknown:
+        #        return True
+        #    if T1 is Unknown:
+        #        return False
+        #    return T1 == T2
+
+        
+        result_dicts = []
+        for Q in egs:
+            d = invariants_dict(Q, include_surface=use_surface)
+            d["Alexander poly"] = Q.alexander_poly()
+            if d not in result_dicts:
+                result_dicts.append(d)
+        return result_dicts
+    acyclic_invariants = []
+    cyclic_invariants = []
+
+
+
